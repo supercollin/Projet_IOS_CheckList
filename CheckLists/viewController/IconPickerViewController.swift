87686a6516ab381bc5661952ab9f0c2dd5ae3623
Unknown <guillaume.colletaz01@gmail.com>
2018-04-05 15:34:59 +0200

    question 74

---
+++ CheckLists/viewController/IconPickerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  IconPickerViewController.swift
+//  CheckLists
+//
+//  Created by iem on 05/04/2018.
+//  Copyright © 2018 iem. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class IconPickerViewController : UITableViewController{
+    
+    var iconList = [IconAsset.Appointments, IconAsset.Birthdays, IconAsset.Chores,
+                    IconAsset.Drinks, IconAsset.Folder, IconAsset.Groceries,
+                    IconAsset.Inbox, IconAsset.Photos, IconAsset.Trips,
+                    IconAsset.NoIcon]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return iconList.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Icon", for: indexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = iconList[row].rawValue
+        cell.imageView?.image = iconList[row].image
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
--- CheckLists/viewController/ListDetailViewController.swift
+++ CheckLists/viewController/ListDetailViewController.swift
@@ -12,9 +12,11 @@ class ListDetailViewController: UITableViewController {
 
     @IBOutlet weak var doneButton: UIBarButtonItem!
     @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var iconList: UIImageView!
     
     var delegate : ListDetailViewControllerDelegate?
     var itemToEdit : CheckList?
+    
     
     @IBAction func cancel(_ sender: Any) {
         delegate?.ListDetailViewControllerDidCancel(self)
@@ -25,6 +27,7 @@ class ListDetailViewController: UITableViewController {
         let item = CheckList(name: textfield.text!, items: listItems)
         
         if(itemToEdit != nil){
+            navigationItem.title = "Edit List"
             item.items = listItems
             delegate?.ListDetailViewController(self, didFinishEditingItem: item)
         }else{
@@ -45,7 +48,12 @@ class ListDetailViewController: UITableViewController {
         if(itemToEdit != nil){
             textfield.text = itemToEdit?.name
             self.doneButton.isEnabled = false
+            self.iconList.image = itemToEdit?.icon.image
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
